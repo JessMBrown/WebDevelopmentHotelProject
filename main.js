fe03c53b12eecf9d15908c97c254ef4250d8b6a3
JessicaBrown <j.p.donovan.brown@gmail.com>
2024-04-07 21:20:01 +0100

    premiunrooms

--- main.js
+++ main.js
@@ -3,15 +3,13 @@ let open = document.querySelector('.navbar--icon');
 let menu = document.querySelector('.nav--open');
 let close = document.querySelector('.nav--open-icon');
 
-
 open.addEventListener('click', function() {
-    menu.classList.toggle('close');
-    });
-
+  menu.classList.toggle('close');
+});
 
 close.addEventListener('click', function() {
-    menu.classList.toggle('close');
-    });
+  menu.classList.toggle('close');
+});
 
 // Scrolling hero images
 // startImageTransition();
@@ -86,3 +84,41 @@ function carousel() {
   x[myIndex-1].style.display = "block";
   setTimeout(carousel, 3000);
 }
+
+// LightBox
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
