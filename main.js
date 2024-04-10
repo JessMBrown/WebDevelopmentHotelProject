e346caa2555f2a851e582edeabc39418d4865c3f
JessicaBrown <j.p.donovan.brown@gmail.com>
2024-04-10 23:27:47 +0100

    added /a on logo

--- main.js
+++ main.js
@@ -177,19 +177,19 @@ function openModal() {
 
 
 
-  let mybutton = document.getElementById("myBtn");
-  window.onscroll = function() {scrollFunction()};
-  function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+  // let mybutton = document.getElementById("myBtn");
+  // window.onscroll = function() {scrollFunction()};
+  // function scrollFunction() {
+  //   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  //     mybutton.style.display = "block";
+  //   } else {
+  //     mybutton.style.display = "none";
+  //   }
+  // }
+  // function topFunction() {
+  //   document.body.scrollTop = 0;
+  //   document.documentElement.scrollTop = 0;
+  // }
  
 
 
